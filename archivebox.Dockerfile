abb7084c95eb2defe304627f68a70e69ee2297a6
carrein <carreinlee@proton.me>
2025-11-10 05:23:15 -0800

    updates.

--- archivebox.Dockerfile
+++
@@ -1,5 +0,0 @@
-# Start with the official ArchiveBox image
-FROM archivebox/archivebox:latest
-
-# Install the latest version of yt-dlp
-RUN pip install --upgrade yt-dlp
--- restic/backup/restic.backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. /zpool/catallenya/restic/restic.conf
-
-# Backup target folders to repository.
-restic -r ${RESTIC_DRIVER}:${RESTIC_RCLONE_REMOTE}:${RESTIC_BACKUP_LOCATION} \
-    --exclude ${RESTIC_EXCLUDE_TARGETS} \
-    --verbose backup ${RESTIC_BACKUP_TARGETS} \
-    --password-file ${RESTIC_PASSWORD_FILE}
